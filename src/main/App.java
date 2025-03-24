d336fac30bf5f451632d932d9a77bbe9a835ff1c
Neider989898 <neiderospina98@gmail.com>
2025-03-24 13:46:42 -0500

    Añadir Jenkinsfile para CI121

--- src/main/App.java
+++ src/main/App.java
@@ -1,6 +1,6 @@
 package com.ejemplo;
 public class App {
     public static void main(String[] args) {
-        System.out.println("¡Hola, Mundo!");
+        System.out.println("¡Hola, Mundo! neider ");
     }
 }
